141be54c7a8da4a7e35f88114f1d17aaa116ffa0
Jason Smigovsky <jsmigovsky@fearless.tech>
2018-03-28 09:57:19 -0400

    the 'run.sh' script now takes argument 1 as the AWS credential profile name to use

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ export IMAGE_NAME="sba-gov-centos7-packer"
 docker image build -t ${IMAGE_NAME} .
 docker container run --rm -i -t \
 -v "/home/${USER}/.aws/:/root/.aws/" \
--e AWS_PROFILE='sba-dev' \
+-e AWS_PROFILE="${1:-default}" \
 -e REGION='us-east-1' \
 -e SSM_KEY='TEST_AMI' \
 -e PACKER_FILE='cis-centos7.json' \
--e TAG='XJASONX-cis-centos7' \
+-e TAG="${USER}-cis-centos7" \
 ${IMAGE_NAME}
 
