e13dd40ffdfa949baf0631aea85d8cef4f78ea6a
Jason Smigovsky <jsmigovsky@fearless.tech>
2020-07-28 10:34:17 -0400

    IA-1549: argument to allow interactive container to start for testing build

--- build.sh
+++ build.sh
@@ -3,4 +3,9 @@ set -uo pipefail
 export IMAGE_NAME="ussba/packer-git-awscli"
 
 docker image pull hashicorp/packer:light
-docker image build -t ${IMAGE_NAME} .
+docker image build -t ${IMAGE_NAME}:build .
+
+
+if [[ ! -z "$1" ]]; then
+  docker container run --rm -i -t ${IMAGE_NAME}:build /bin/bash
+fi
